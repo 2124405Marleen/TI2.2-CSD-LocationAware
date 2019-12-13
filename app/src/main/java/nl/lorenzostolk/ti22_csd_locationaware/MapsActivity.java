4020aaa6b11b0d4a874461f81a4b8ea210a058eb
Lorenzo <lrstolk@avans.nl>
2019-12-13 14:08:10 +0100

    added places

--- app/src/main/java/nl/lorenzostolk/ti22_csd_locationaware/MapsActivity.java
+++ app/src/main/java/nl/lorenzostolk/ti22_csd_locationaware/MapsActivity.java
@@ -11,9 +11,15 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
+    private ArrayList<Place> avansBuildings;
+    private ArrayList<Place> drinkPlaces;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,6 +29,9 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+
+        avansBuildings = new ArrayList<>();
+        initPlaces();
     }
 
 
@@ -40,8 +49,48 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         mMap = googleMap;
 
         // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+//        LatLng sydney = new LatLng(-34, 151);
+//        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+//        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        mMap.getUiSettings().setMapToolbarEnabled(false);
+
+        addMarkers();
+
     }
+
+    private void initPlaces() {
+        Collections.addAll(avansBuildings,
+                new Place("Avans H", new LatLng(51.584018, 4.797174),
+                        "https://punt.avans.nl/wp-content/uploads/2016/08/HogeschoollaanBreda-1920.jpg",
+                        "Hogeschoollaan main building. "),
+
+                new Place("Avans HQ", new LatLng(51.584565, 4.798295),
+                        "https://gebouwen.avans.nl/binaries/content/gallery/iavans/thematic.gebouwen/quadrium/bouwfotos/12-1/01_20170112_104732_web.jpg",
+                        "Quadrium building. "),
+
+                new Place("Avans LA", new LatLng(51.585765, 4.792273),
+                        "https://gebouwen.avans.nl/binaries/content/gallery/iavans/thematic.gebouwen/lovensdijkstraat/3693_web.jpg",
+                        "Lovensdijk A building. "),
+
+                new Place("Avans LC", new LatLng(51.586038, 4.793533),
+                        "https://scontent-yyz1-1.cdninstagram.com/vp/06ffdc03e320bc7d07385c7519049295/5E2CD9C4/t51.2885-15/sh0.08/e35/s640x640/47223033_194201228112483_4394909092588572764_n.jpg?_nc_ht=scontent-yyz1-1.cdninstagram.com&_nc_cat=103",
+                        "Lovensdijk C building. "),
+
+                new Place("Avans LD"
+                        , new LatLng(51.585775, 4.794450),
+                        "https://gebouwen.avans.nl/binaries/content/gallery/iavans/thematic.gebouwen/lovensdijkstraat/3711_web.jpg",
+                        "Lovensdijk D building. ")
+        );
+    }
+
+    private void addMarkers() {
+
+        for (Place p : avansBuildings
+        ) {
+            mMap.addMarker(new MarkerOptions().position(p.getLatLng()).title(p.getDescription()));
+        }
+
+    }
+
+
 }
---
+++ app/src/main/java/nl/lorenzostolk/ti22_csd_locationaware/Place.java
@@ -0,0 +1,49 @@
+package nl.lorenzostolk.ti22_csd_locationaware;
+
+import com.google.android.gms.maps.model.LatLng;
+
+public class Place {
+    private String name;
+    private LatLng latLng;
+    private String imageURL;
+    private String description;
+
+    public Place(String name, LatLng latLng, String imageURL, String description) {
+        this.name = name;
+        this.latLng = latLng;
+        this.imageURL = imageURL;
+        this.description = description;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public LatLng getLatLng() {
+        return latLng;
+    }
+
+    public void setLatLng(LatLng latLng) {
+        this.latLng = latLng;
+    }
+
+    public String getImageURL() {
+        return imageURL;
+    }
+
+    public void setImageURL(String imageURL) {
+        this.imageURL = imageURL;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
