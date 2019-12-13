10b7c9c5c440889d5381dacd09fa067b6402530c
Marleen <mroubos2@avans.nl>
2019-12-13 14:08:47 +0100

    main activity

--- app/src/main/java/nl/lorenzostolk/ti22_csd_locationaware/MainActivity.java
+++ app/src/main/java/nl/lorenzostolk/ti22_csd_locationaware/MainActivity.java
@@ -2,13 +2,29 @@ package nl.lorenzostolk.ti22_csd_locationaware;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button buttonToMap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initButtonToMap();
+    }
+
+    public void initButtonToMap(){
+        buttonToMap = findViewById(R.id.main_button_map);
+        buttonToMap.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, MapsActivity.class));
+            }
+        });
     }
 }
