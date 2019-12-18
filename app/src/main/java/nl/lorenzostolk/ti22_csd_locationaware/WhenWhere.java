473dbfba892bf61b5ebf4f36fad96b66d80f7a67
Marleen <mroubos2@avans.nl>
2019-12-18 14:32:02 +0100

    Style fix en actionbar remove

--- app/src/main/java/nl/lorenzostolk/ti22_csd_locationaware/WhenWhere.java
+++ app/src/main/java/nl/lorenzostolk/ti22_csd_locationaware/WhenWhere.java
@@ -47,7 +47,7 @@ public class WhenWhere implements Serializable {
     public int getTotalSecondsSpendDuringStay() {
         return totalSecondsSpendDuringStay;
     }
-    
+
 
     @RequiresApi(api = Build.VERSION_CODES.O)
     public String getTimeSpend() {
